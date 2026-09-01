3651114265318d955e7fb9315feb6558f2ffe174
gupta-ronak12 <ronak.gupta2427@gmail.com>
2026-09-02 00:54:25 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.routers.feature_flags import router as feature_flags_router
 from app.routers.auth import router as auth_router
+from app.routers.environment_override import router as environment_override_router
+from app.routers import flag_evaluation
+from app.routers import environments
 
 app = FastAPI(
     title="Feature Management System",
@@ -9,7 +12,9 @@ app = FastAPI(
 )
 app.include_router(feature_flags_router)
 app.include_router(auth_router)
-
+app.include_router(environment_override_router)
+app.include_router(flag_evaluation.router)
+app.include_router(environments.router)
 @app.get("/")
 def root():
     return {"message": "Feature Management System API is running"}
